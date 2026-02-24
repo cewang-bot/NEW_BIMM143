2241cf191ad8fcd2386dda4c768ea220d7b8f61c
Cecilia Wang <cewang@ucsd.edu>
2026-02-24 12:27:20 -0800

    add class 4

---
+++ class04/Class04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Class 4 R Intro"
+#' author: "Yushi Wang"
+#' date: "Jan 15, 2026"
+#' ---
+#' 
+x <- 1:50
+plot(x,sin(x))
+plot(x,sin(x), typ="l")
+plot(x,sin(x), typ="l",col="pink", lwd=3)
+log(10,base=10) 
+
